f268b88a8d058aeb1febddf9c0d33d1e1f407a00
Francis Chen <fncis.chen@gmail.com>
2019-04-01 14:09:10 -0700

    refactored server

--- auth/server.js
+++ auth/server.js
@@ -2,46 +2,42 @@ const bcrypt = require("bcryptjs");
 const express = require("express");
 const server = express();
 
-const db = require("../dev.sqlite3");
-const Users = require("../data/usersDataModel.js");
+const Users = require("../data/users/usersDataModel.js");
 
 // https://cloud.google.com/blog/products/gcp/12-best-practices-for-user-account
-
+// https://medium.com/@paulrohan/how-bcryptjs-works-90ef4cb85bf4
 server.use(express.json());
 
 server.post("/api/register", (req,res) => {
-    const credentials = req.body;
-    const hash = bcrypt.hashSync(credentials.password, 14);
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 14);
 
     // use hash to override the password
     credentials.password = hash;
 
     // questions about hash 
-    Users.add(credentials)
+    Users.add(user)
         .then(saved => {
-            
+            res.status(200).json(saved);
         })
         .catch(error => {
-
+            res.status(500).json({errorMessage: "There was an error in registering your user."})
         })
 
 })
 
 server.post("/api/login", (req,res) => {
 
-    const credentials = req.body;
+    let {username, password} = req.body;
     // or let {username, password} = req.body
-    const hash = bcrypt.hashSync(credentials.password, 14);
+    const hash = bcrypt.hashSync(user.password, 14);
 
 
-    Users.findBy({ credentials }).first().then(
+    Users.findBy({ user }).first().then(
         user => {
-            if(user && bcrypt.compareSync(credientals.password, user.password)) 
+            if(user && bcrypt.compareSync(password, user.password)) 
             {
-
-                // send to header authorization 
-                res.cookie()
-                res.status().json({message: `Welcome ${credentials.username}`})
+                res.status(200).json({message: `Welcome ${user}`})
             }
             else {
                 res.status(401).json({message: 'Invalid Credentials'});
@@ -56,15 +52,10 @@ server.post("/api/login", (req,res) => {
 
 // restrict access to this endpoint to users that only provide the right credentials in the headers
 // https://flaviocopes.com/express-headers/
-server.get("/api/users", restricted, roles(['sales', 'marketing',]) (req,res) => {
-    if(!req.headers.username) {
-        return res.status(401).json({errorMessage: "You need to be logged in to access these users"})
-    }
-    else(
-        Users.Get()
-            .then( users => res.status(200).json())
-            .catch() 
-    )
+server.get("/api/users", restricted,(req,res) => {
+    Users.get()
+         .then( users => res.status(200).json())
+         .catch() 
 })
 
 function restricted(req, res, next) {
